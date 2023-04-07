667901cc0c7cc7d59bc893713a3426a7f9267874
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-04-07 21:34:04 +0200

    break increment decremet functions

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 export default function Session() {
+  const [sessionLength, setSessionLength] = useState(25);
   return (
     <div id="session-label">
       <p>Session Length</p>
       <button id="session-increment"></button>
       <button id="session-decrement"></button>
+      <p id="session-length">{sessionLength}</p>
     </div>
   );
 }
--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 export default function TimeLeft() {
   return (
-    <>
+    <div>
       <p>Time Left Test</p>
-    </>
+      <p id="timer-label">"Session"</p>
+    </div>
   );
 }
