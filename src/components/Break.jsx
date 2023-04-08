ddd62819c07fb22778686b85db314549bb4a40f9
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-04-08 17:48:53 +0200

    imported moment package to change time units

--- src/components/Break.jsx
+++ src/components/Break.jsx
@@ -1,12 +1,35 @@
+import moment from "moment";
 import React, { useState } from "react";
 export default function Break() {
   const [breakLengthInSeconds, setBreakLengthInSeconds] = useState(5);
+
+  const decrementBreakLengthInSeconds = () => {
+    const newBreakLengthInSeconds = breakLengthInSeconds - 1;
+    if (breakLengthInSeconds < 0) {
+      setBreakLengthInSeconds(8);
+    } else {
+      setBreakLengthInSeconds(newBreakLengthInSeconds);
+    }
+  };
+
+  const incrementBreakLengthInSeconds = () => {
+    setBreakLengthInSeconds(breakLengthInSeconds + 1);
+  };
+
+  const breakLengthInMinutes = moment
+    .duration(breakLengthInSeconds, "s")
+    .minutes();
+
   return (
     <div>
-      <p>Hello World Test</p>
+      <p>BreakLabel</p>
 
-      <button id="break-decrement"></button>
-      <button id="break-increment"></button>
+      <button id="break-decrement" onClick={decrementBreakLengthInSeconds}>
+        -
+      </button>
+      <button id="break-increment" onClick={incrementBreakLengthInSeconds}>
+        +
+      </button>
       <p id="break-length">{breakLengthInSeconds}</p>
     </div>
   );
